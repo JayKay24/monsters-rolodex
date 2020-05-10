2e209010da5d33f5402b3cf5d33c8ab2b9a1edfb
James Kinyua <jameskinyua590@gmail.com>
2020-05-10 21:42:05 +0300

    Updates

---
+++ precache-manifest.1e85c5bf3e55cf7411b9f7b3db221bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b8be8e99bda428673759ec4dbaf09d",
+    "url": "/JayKay24/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "28cdfa913d324d5e908b",
+    "url": "/JayKay24/monsters-rolodex/static/css/main.d9ae10ba.chunk.css"
+  },
+  {
+    "revision": "28dd644d0e19315f9949",
+    "url": "/JayKay24/monsters-rolodex/static/js/2.9d0c4314.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JayKay24/monsters-rolodex/static/js/2.9d0c4314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28cdfa913d324d5e908b",
+    "url": "/JayKay24/monsters-rolodex/static/js/main.e7d632fd.chunk.js"
+  },
+  {
+    "revision": "d273ffedc5949c187719",
+    "url": "/JayKay24/monsters-rolodex/static/js/runtime-main.90d0dfe9.js"
+  }
+]);
